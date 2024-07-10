8bdfdbcb32f2c570d89cf37aab4b7054d810c110
NitoTech <95981007+ItzKasmi@users.noreply.github.com>
2024-07-10 14:55:54 -0400

    Added Working API Fetch
    Adding a way to collect all API data to then deconstruct and be sent out to each individual react component.

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,6 @@ import Descendant from "./components/Descendant"
 import StatComponent from "./components/StatComponent"
 import Weapon from "./components/Weapon"
 
-const NICKNAME = "NitoTech%230254";
-const OUID = "8102e8f67c7128b13587299ded26367ba42bb9591fc1dcdf29fba0635144751d";
-
-
-
 // fetchAPIData();
 
 function App() {
--- src/api.jsx
+++ src/api.jsx
@@ -1,10 +1,123 @@
 export async function gatherAPIData() {
-  const TFD_KEY = import.meta.env.VITE_TFD_API_KEY
-  fetchBasicAPIData(TFD_KEY);
+  const TFD_KEY = import.meta.env.VITE_TFD_API_KEY;
+  const OUID = "8102e8f67c7128b13587299ded26367ba42bb9591fc1dcdf29fba0635144751d";
+  const NICKNAME = "NitoTech%230254";
+  
+  const basicAPIData = fetchBasicAPIData(TFD_KEY, OUID);
+  const descendantAPIData = fetchDescendantAPIData(TFD_KEY, OUID);
+  const weaponAPIData = fetchWeaponAPIData(TFD_KEY, OUID);
+  const reactorAPIData = fetchReactorAPIData(TFD_KEY, OUID);
+  const componentAPIData = fetchComponentAPIData(TFD_KEY, OUID);
+
+  return basicAPIData, descendantAPIData, weaponAPIData, reactorAPIData, componentAPIData;
 }
 
-async function fetchBasicAPIData(api_key) {
+
+`
+Used Basic API Data:
+  - "user_name"
+  - "platform_type"
+  - "mastery_rank_level"
+  Maybe get titles not sure yet
+`
+async function fetchBasicAPIData(api_key, OUID) {
   const url = `https://open.api.nexon.com/tfd/v1/user/basic?ouid=${OUID}`
+  try {
+    const res = await fetch(url, {
+      headers: {
+        "x-nxopen-api-key": api_key
+      }
+    })
+    const apiData = await res.json();
+    console.log(apiData);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+
+`
+Used Descendant API Data:
+  - "descendant_id"
+  - "descendant_level"
+  If I show off individual modules will use:
+    - "module_max_capacity"
+    - "module_capacity"
+    - "module"
+`
+async function fetchDescendantAPIData(api_key, OUID) {
+  const url = `https://open.api.nexon.com/tfd/v1/user/descendant?ouid=${OUID}`
+  try {
+    const res = await fetch(url, {
+      headers: {
+        "x-nxopen-api-key": api_key
+      }
+    })
+    const apiData = await res.json();
+    console.log(apiData);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+
+`
+Used Weapon API Data:
+  - "weapon"
+    - "weapon_id"
+    - "weapon_level"
+
+  Will need to find a way to pull from downloaded metadata JSON based on weapon_id
+  for image urls.
+`
+async function fetchWeaponAPIData(api_key, OUID) {
+  const url = `https://open.api.nexon.com/tfd/v1/user/weapon?language_code=en&?ouid=${OUID}`
+  try {
+    const res = await fetch(url, {
+      headers: {
+        "x-nxopen-api-key": api_key
+      }
+    })
+    const apiData = await res.json();
+    console.log(apiData);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+
+`
+Used Reactor API Data:
+  - "reactor_id"
+  - "reactor_level"
+`
+async function fetchReactorAPIData(api_key, OUID) {
+  const url = `https://open.api.nexon.com/tfd/v1/user/reactor?language_code=en&?ouid=${OUID}`
+  try {
+    const res = await fetch(url, {
+      headers: {
+        "x-nxopen-api-key": api_key
+      }
+    })
+    const apiData = await res.json();
+    console.log(apiData);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+
+`
+Used Component API Data:
+  - "external_component"
+    - "external_component_id"
+    - "external_component_level"
+    - "external_component_additional_stat"
+      - "additional_stat_name"
+      - "additional_stat_value"
+`
+async function fetchComponentAPIData(api_key, OUID) {
+  const url = `https://open.api.nexon.com/tfd/v1/user/external-component?language_code=en&?ouid=${OUID}`
   try {
     const res = await fetch(url, {
       headers: {
