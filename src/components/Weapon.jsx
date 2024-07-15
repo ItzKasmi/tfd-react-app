a6e3b32569b98f4d4a8bbef97afc03681c515c8f
NitoTech <95981007+ItzKasmi@users.noreply.github.com>
2024-07-15 10:42:12 -0400

    Update Weapon.jsx
    Potential way to display image data

--- src/components/Weapon.jsx
+++ src/components/Weapon.jsx
@@ -2,14 +2,38 @@ import { useEffect, useState } from "react"
 import React from 'react'
 
 export default function Weapon(props) {
+  const [ imageUrl, setImageUrl] = useState(null);
   const { weaponData } = props;
 
-  const jsonFilePath = "public\weapon_data.json"
+  const jsonFilePath = `${process.env.PUBLIC_URL}/weapon_data.json`;
+  const fetchWeaponImageData = async () => {
+    try {
+      const res = await fetch(jsonFilePath)
+      const jsonData = await res.json();
+      console.log(jsonData);
+      return jsonData;
+    } catch (err) {
+      console.log(err.message);
+      throw err;
+    }
+  }
 
   const getImageUrl = (weaponId) => {
-    const weapon = weaponData.find((weapon) => weapon.weapon_id === weaponId);
+    const jsonWeaponData = fetchWeaponImageData();
+    const weapon = jsonWeaponData.find((weapon) => weapon.weapon_id === weaponId);
     return weapon ? weapon.image_url : null;
+  }; 
+
+  const handleFetchImage = () => {
+    const url = getImageUrl(weaponId);
+    if (url) {
+      setImageUrl(url);
+    } else {
+      console.error('Weapon not found or missing image URL');
+    }
   };
+
+  handleFetchImage();
 
   if (!weaponData) {
     return (
@@ -21,7 +45,7 @@ export default function Weapon(props) {
 
   return (
     <div>
-      {weaponData.weapon[0].weapon_id}
+      {imageUrl && <img src={imageUrl} alt={`Weapon ${weaponData.weapon[0].weapon_id}`} />}
     </div>
   )
 }
