950a089725185e6f1bff8babf3e1d722c971a0ae
NitoTech <95981007+ItzKasmi@users.noreply.github.com>
2024-07-15 12:15:31 -0400

    Working Weapon Image Fetch
    Made a way so that once the api fetches the weapon data, it then parses a large JSON metadata file and pulls out the weapon_image that corresponds to the weapon_id. Then displays it on the webpage.

--- src/components/Weapon.jsx
+++ src/components/Weapon.jsx
@@ -1,51 +1,55 @@
-import { useEffect, useState } from "react"
-import React from 'react'
+import { useEffect, useState } from "react";
+import React from 'react';
 
 export default function Weapon(props) {
-  const [ imageUrl, setImageUrl] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
   const { weaponData } = props;
 
-  const jsonFilePath = `${process.env.PUBLIC_URL}/weapon_data.json`;
+  const jsonFilePath = `weapon_data.json`;
+
   const fetchWeaponImageData = async () => {
     try {
-      const res = await fetch(jsonFilePath)
+      const res = await fetch(jsonFilePath);
       const jsonData = await res.json();
-      console.log(jsonData);
       return jsonData;
     } catch (err) {
       console.log(err.message);
       throw err;
     }
-  }
-
-  const getImageUrl = (weaponId) => {
-    const jsonWeaponData = fetchWeaponImageData();
-    const weapon = jsonWeaponData.find((weapon) => weapon.weapon_id === weaponId);
-    return weapon ? weapon.image_url : null;
-  }; 
-
-  const handleFetchImage = () => {
-    const url = getImageUrl(weaponId);
-    if (url) {
-      setImageUrl(url);
-    } else {
-      console.error('Weapon not found or missing image URL');
-    }
   };
 
-  handleFetchImage();
+  const getImageUrl = async (weaponId) => {
+    const jsonWeaponData = await fetchWeaponImageData();
+    const weapon = jsonWeaponData.find((weapon) => weapon.weapon_id === weaponId);
+    return weapon ? weapon.image_url : null;
+  };
+
+  useEffect(() => {
+    const handleFetchImage = async () => {
+      if (weaponData && weaponData.weapon && weaponData.weapon.length > 0) {
+        const url = await getImageUrl(weaponData.weapon[0].weapon_id);
+        if (url) {
+          setImageUrl(url);
+        } else {
+          console.error('Weapon not found or missing image URL');
+        }
+      }
+    };
+
+    handleFetchImage();
+  }, [weaponData]);
 
   if (!weaponData) {
     return (
       <div>
         <p>Error, no data loaded in weapon.</p>
       </div>
-    )
+    );
   }
 
   return (
     <div>
       {imageUrl && <img src={imageUrl} alt={`Weapon ${weaponData.weapon[0].weapon_id}`} />}
     </div>
-  )
+  );
 }
